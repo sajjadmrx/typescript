98ea0a686d6793cc78d6c4fc636e4719855f30e0
sajjadmrx <sajjadzibaee102@gmail.com>
2021-10-10 22:22:29 -0700

    install and run typescript

---
+++ install-and-run-typescript/build/script.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    var firstname = "sajjad";
+    var lastname = "mrx";
+    var fullname = firstname + "-" + lastname;
+    document.getElementById("name").innerHTML = "Hello " + fullname;
+}
---
+++ install-and-run-typescript/src/script.ts
@@ -0,0 +1,8 @@
+{
+    const firstname: string = "sajjad";
+    const lastname: string = "mrx";
+    const fullname: string = `${firstname}-${lastname}`;
+
+    console.log(fullname);
+    document.getElementById("")
+}
