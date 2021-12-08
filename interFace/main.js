76f3d314390da72d28f8d4b4a84db0f05dbe9d68
sajjadmrx <sajjadzibaee102@gmail.com>
2021-12-08 19:55:13 +0330

    functionOverload & update interface

--- interFace/main.js
+++ interFace/main.js
@@ -1,14 +1,45 @@
-var teacher = {
-    firstName: 'John',
-    lastName: 'Doe'
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+var db = [];
+var database = /** @class */ (function () {
+    function database() {
+    }
+    database.prototype.save = function (person) {
+        var user = db.find(function (v) { return v.name == person.name; });
+        if (user) {
+            return user;
+        }
+        person.id = db.length + 1;
+        db.push(person);
+        return person;
+    };
+    database.prototype["delete"] = function (person) {
+        db.splice(db.indexOf(person), 1);
+    };
+    database.prototype.getPersonById = function (id) {
+        return db.find(function (v) { return v.id == id; });
+    };
+    database.prototype.getAllPersons = function () {
+        return db;
+    };
+    database.prototype.getPersonsByAge = function (age) {
+        return db.filter(function (v) { return v.age == age; });
+    };
+    return database;
+}());
+var dbService = new database();
+var person1 = {
+    name: 'sajjad',
+    family: 'mrx',
+    age: 19,
+    email: 'fake@gmail.com'
 };
-var student = {
-    firstName: 'jack',
-    lastName: 'Johnson'
+var person2 = {
+    name: 'Ali',
+    family: 'Zareh',
+    age: 25
 };
-var jack = {
-    firstName: 'sajjad',
-    lastName: "mrx",
-    age: 19
-};
-console.log(teacher, student);
+dbService.save(person1);
+dbService.save(person2);
+console.log(dbService.getAllPersons());
--- interFace/main.ts
+++ interFace/main.ts
@@ -1,29 +1,100 @@
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+
+// }
+
+// const teacher: Person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// const student: Person = {
+//     firstName: 'jack',
+//     lastName: 'Johnson',
+// };
+
+// interface Boy extends Person {
+//     age: number;
+// }
+// const jack: Boy = {
+//     firstName: 'sajjad',
+//     lastName: "mrx",
+//     age: 19
+// }
+
+
+
+
+// console.log(teacher, student);
+
+
+
 interface Person {
-    firstName: string;
-    lastName: string;
-
+    id?: number;
+    name: string;
+    family: string;
+    age: number,
+    email?: string
 }
+const db: any[] = []
+interface PersonService {
+    save(person: Person): Person;
+    delete(person: Person): void;
+    getPersonById(id: number): Person | undefined;
+    getAllPersons(): Person[];
 
-const teacher: Person = {
-    firstName: 'John',
-    lastName: 'Doe'
-};
-
-const student: Person = {
-    firstName: 'jack',
-    lastName: 'Johnson',
-};
-
-interface Boy extends Person {
-    age: number;
-}
-const jack: Boy = {
-    firstName: 'sajjad',
-    lastName: "mrx",
-    age: 19
 }
 
 
 
+class database implements PersonService {
 
-console.log(teacher, student);
+    save(person: Person): Person {
+        const user = db.find(v => v.name == person.name)
+        if (user) {
+            return user
+        }
+        person.id = db.length + 1
+        db.push(person)
+        return person
+    }
+
+    delete(person: Person): void {
+        db.splice(db.indexOf(person), 1)
+    }
+
+    getPersonById(id: number): Person | undefined {
+        return db.find(v => v.id == id)
+    }
+
+    getAllPersons(): Person[] {
+        return db
+    }
+
+    getPersonsByAge(age: number): Person[] | undefined {
+        return db.filter(v => v.age == age)
+
+    }
+
+
+}
+
+
+const dbService = new database()
+let person1: Person = {
+    name: 'sajjad',
+    family: 'mrx',
+    age: 19,
+    email: 'fake@gmail.com'
+}
+let person2: Person = {
+    name: 'Ali',
+    family: 'Zareh',
+    age: 25,
+}
+dbService.save(person1)
+dbService.save(person2)
+
+console.log(dbService.getAllPersons())
+
